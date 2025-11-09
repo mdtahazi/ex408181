dd150bd58ed3e9443824196b3801062e400bc738
taha <tt602271tt@gmail.com>
2025-11-09 12:04:55 +0330

    Add files via upload

---
+++ task.php
@@ -0,0 +1,47 @@
+<?php
+
+abstract class shape {
+    const UNIT = "cm" ;
+
+    abstract public function area();
+}
+
+class Rectangle extends shape{
+    public $width;
+    public $height;
+
+    public function __construct($width, $height){
+        $this->width = $width;
+        $this->height = $height;
+    }
+    
+    public function area(){
+        return "The area of ​​the rectangle : " . $this->width * $this->height . self::UNIT . "<sup>2</sup>";
+    }
+}
+
+class Circle extends shape{
+    public $radius;
+    
+    public function __construct($radius){
+        $this->radius = $radius;
+    }
+
+    public function area(){
+        return "The area of ​​the circle : " . pi() * pow($this->radius,2) . self::UNIT . "<sup>2</sup>";
+    }
+}
+
+
+$rectangle = new Rectangle(5,6);
+echo $rectangle->area();
+echo "<br>";
+
+
+$circle = new Circle(34);
+echo $circle->area();
+
+
+
+
+
